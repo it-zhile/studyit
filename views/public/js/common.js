b76916f9f94d8954b418fb53658ce0e2c3cb4eb4
everest <252028577@qq.com>
2017-08-14 22:08:38 +0800

    登陆成功之后渲染主页面的用户和头像信息

--- views/public/js/common.js
+++ views/public/js/common.js
@@ -9,4 +9,13 @@
 
 if (!$.cookie('PHPSESSID') && location.pathname != '/login') {
 	location.href = '/login';
+};
+
+// var htmlStr = template('tpl_aside_profile',JSON.parse($.cookie('tcinfo')));
+// $('.aside>.profile').html(htmlStr);
+
+if(location.pathname!='/login'&&location.pathname!='/dashboard/login'&&location.pathname!='/views/dashboard/login'){
+    //需要渲染模板
+    var htmlStr = template('tpl_aside_profile',JSON.parse($.cookie('tcinfo')));
+     $('.aside>.profile').html(htmlStr);
 }
